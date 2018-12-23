5d7d42bb5ba1d69a22bfb205b211d92d4dc3b577
ccc <ccc@webapiz.com>
2018-12-23 20:32:57 +0800

    异步非阻塞 Servlet 代码示例

--- lspringb/src/main/java/com/lspringb/web/servlet/MyServlet.java
+++ lspringb/src/main/java/com/lspringb/web/servlet/MyServlet.java
@@ -2,19 +2,31 @@ package com.lspringb.web.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/my/servlet")
+@WebServlet(urlPatterns = "/my/servlet",asyncSupported=true)
 public class MyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	        throws ServletException, IOException
 	{
-	        resp.getWriter().println("hello,world123");
+		    AsyncContext asyncContext = req.startAsync();
+		    
+		    asyncContext.start(()->{
+	            try {
+	                resp.getWriter().println("Hello,World async");
+	                // 触发完成
+	                asyncContext.complete();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        });
+	        //resp.getWriter().println("hello,world123");
 	}
 	
 }
