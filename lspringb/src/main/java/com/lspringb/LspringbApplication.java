93418e4c89431d1439e310f0bec91892a1cc7659
ccc <ccc@webapiz.com>
2018-12-23 19:57:20 +0800

    使用eclipse初始化spring-boot

---
+++ lspringb/src/main/java/com/lspringb/LspringbApplication.java
@@ -0,0 +1,14 @@
+package com.lspringb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LspringbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LspringbApplication.class, args);
+	}
+
+}
+
