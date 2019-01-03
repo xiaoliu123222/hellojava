3957e2f82816f5d2d76deb74ee271d083dcd145c
ccc <ccc@webapiz.com>
2019-01-03 20:52:13 +0800

    youfan:新建项目

---
+++ youfanshop/youfanshopuser/src/main/java/com/youfan/YoufanshopuserApplication.java
@@ -0,0 +1,14 @@
+package com.youfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoufanshopuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoufanshopuserApplication.class, args);
+	}
+
+}
+
