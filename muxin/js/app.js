e700c7ed9a0ba467e27f5bb4588d2291ee7ee6ea
ccc <ccc@webapiz.com>
2019-01-15 10:57:08 +0800

    muxin:前端注册登录页面

---
+++ muxin/js/app.js
@@ -0,0 +1,51 @@
+window.app = {
+	
+	serverUrl: 'http://192.168.23.1:8080',
+	
+	/**
+	 * 判断字符串是否为空
+	 * @param {Object} str
+	 * true：不为空
+	 * false：为空
+	 */
+	isNotNull: function(str) {
+		if (str != null && str != "" && str != undefined) {
+			return true;
+		}
+		return false;
+	},
+	
+	/**
+	 * 保存用户的全局对象
+	 */
+	setUserGlobalInfo: function(user) {
+		var userInfoStr = JSON.stringify(user);
+		plus.storage.setItem("userInfo",userInfoStr);
+	},
+	
+	/**
+	 * 获取用户的全局对象
+	 */
+	getUserGlobalInfo: function() {
+		var userInfoStr = plus.storage.getItem("userInfo");
+		return JSON.parse(userInfoStr);
+	},
+	
+	userLogout: function() {
+		plus.storage.removeItem("userInfo");
+	},
+		/**
+	 * 封装消息提示框，默认mui的不支持居中和自定义icon，所以使用h5+
+	 * @param {Object} msg
+	 * @param {Object} type
+	 */
+	showToast: function(msg, type) {
+		plus.nativeUI.toast(msg, 
+			{icon: "image/" + type + ".png", verticalAlign: "center"})
+	}
+	
+	
+	
+	
+	
+}
